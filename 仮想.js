80a949203fdbaa14794203c15b34290860e25b02
naomasamanz <naomasamanz@gmail.com>
2026-02-08 13:13:32 +0900

    Create 仮想.js

---
+++ 仮想.js
@@ -0,0 +1,8 @@
+const virtualEQ = {
+  maxIntensity: "6強",          // 最大震度
+  prefecture: "茨城県沖",      // 震源地
+  magnitude: 7.3,              // マグニチュード
+  depth: 10,                   // 深さ（km）
+  time: new Date().toISOString(),
+  isEEW: true                  // 緊急地震速報フラグ
+};
